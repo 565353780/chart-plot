4b1d844a9862621198b93c4e9283d9e935d71687
565353780 <565353780@qq.com>
2022-01-23 13:18:44 +0800

    now can auto render all data

--- DataPlot.py
+++ DataPlot.py
@@ -5,31 +5,31 @@ import json
 from ChartPlot import ChartPlot
 
 if __name__ == "__main__":
-    f = open("./coscan_data/scene_recovery_rate.txt", "r")
-    data = json.load(f)
-    f.close()
-
-    ours_value = data["Ours"]
-
+    json_file_path = "./coscan_data/scene_recovery_rate.txt"
     x_start = 0
-    x_num = len(ours_value)
     x_step = 1
     fit_polyline = False
     show_confidence_interval = True
     confidence_diff_min = 5
     confidence_diff_max = 10
     show_line_label = True
-    show_confidence_interval_label = True
+    show_confidence_interval_label = False
 
     chart_plot = ChartPlot()
 
-    chart_plot.addLine(
-        x_start, x_num, x_step,
-        "r:", 5, "Ours", fit_polyline,
-        show_confidence_interval,
-        confidence_diff_min, confidence_diff_max)
-    for i in range(len(ours_value)):
-        chart_plot.addPoint(0, i, ours_value[i])
+    data = {}
+    with open(json_file_path, "r") as f:
+        data = json.load(f)
+    for key in data.keys():
+        key_data = data[key]
+        x_num = len(key_data)
+        chart_plot.addLine(
+            x_start, x_num, x_step,
+            "r:", 5, key, fit_polyline,
+            show_confidence_interval,
+            confidence_diff_min, confidence_diff_max)
+        for i in range(len(key_data)):
+            chart_plot.addPoint(0, i, key_data[i])
 
     chart_plot.renderLine(
         show_line_label,
